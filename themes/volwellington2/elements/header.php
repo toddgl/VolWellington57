ad15115fa247f55b9645ff75d18879d056972da9
Glenn Todd <g.todd@internet.co.nz>
2019-11-22 14:02:53 +1300

    Changed Google Analytics tag code at Jo Nixon's request

--- themes/volwellington2/elements/header.php
+++ themes/volwellington2/elements/header.php
@@ -6,14 +6,14 @@ defined('C5_EXECUTE') or die('Access Denied.')
 	<head>
 		<!-- Global site tag (gtag.js) - Google Analytics -->
 		<?php if (Config::get('concrete.env.name') == 'prod') { ?>
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110232753-1"></script>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-146125363-1"></script>
 		<?php } ?>
 		<script>
 		  window.dataLayer = window.dataLayer || [];
 		  function gtag(){dataLayer.push(arguments);}
 		  gtag('js', new Date());
 
-		  gtag('config', 'UA-110232753-1');
+		  gtag('config', 'UA-146125363-1');
 		</script>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
